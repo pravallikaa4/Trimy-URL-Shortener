37c8e3f92af81743e6c469200e9c47ddcf4c9ad1
pravallikaa4 <m.prav1759@gmail.com>
2025-10-04 19:11:15 +0530

    Update page.js

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -3,7 +3,7 @@ import clientPromise from "@/lib/mongodb";
 export default async function Page({params}) {
     const shorturl=(await params.shorturl).toString();
     const client= await clientPromise;
-    const db= client.db("trimly");
+    const db= client.db("Trimly");
     const collection= db.collection("url");
     const doc= await collection.findOne({shorturl: shorturl});
     if(doc){
